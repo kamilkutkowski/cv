423fe97d1a49140bafaadae7ebc0550725b570bb
Kamil Kutkowski <kam.kutkowski@gmail.com>
2026-05-21 09:07:51 +0200

    fix SEO in cv-web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "161999617" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2804245196" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
